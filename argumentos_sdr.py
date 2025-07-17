51841ddf7046886ba97546f0ad0c20b21861577c
Cristian Brenes <49688931+cbrenesgeo@users.noreply.github.com>
2025-07-17 09:07:15 -0600

    Add files via upload

---
+++ argumentos_sdr.py
@@ -0,0 +1,41 @@
+Help on function execute in module natcap.invest.sdr.sdr:
+
+execute(args)
+    Sediment Delivery Ratio.
+    
+    This function calculates the sediment export and retention of a landscape
+    using the sediment delivery ratio model described in the InVEST user's
+    guide.
+    
+    Args:
+        args['workspace_dir'] (string): output directory for intermediate,
+            temporary, and final files
+        args['results_suffix'] (string): (optional) string to append to any
+            output file names
+        args['dem_path'] (string): path to a digital elevation raster
+        args['erosivity_path'] (string): path to rainfall erosivity index
+            raster
+        args['erodibility_path'] (string): a path to soil erodibility raster
+        args['lulc_path'] (string): path to land use/land cover raster
+        args['watersheds_path'] (string): path to vector of the watersheds
+        args['biophysical_table_path'] (string): path to CSV file with
+            biophysical information of each land use classes.  contain the
+            fields 'usle_c' and 'usle_p'
+        args['threshold_flow_accumulation'] (number): number of upslope pixels
+            on the dem to threshold to a stream.
+        args['k_param'] (number): k calibration parameter
+        args['sdr_max'] (number): max value the SDR
+        args['ic_0_param'] (number): ic_0 calibration parameter
+        args['drainage_path'] (string): (optional) path to drainage raster that
+            is used to add additional drainage areas to the internally
+            calculated stream layer
+        args['l_max'] (number): the maximum allowed value of the slope length
+            parameter (L) in the LS factor. If the calculated value of L
+            exceeds 'l_max' it will be clamped to this value.
+        args['n_workers'] (int): if present, indicates how many worker
+            processes should be used in parallel processing. -1 indicates
+            single process mode, 0 is single process but non-blocking mode,
+            and >= 1 is number of processes.
+    
+    Returns:
+        None.
